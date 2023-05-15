495a8b9b0d2f3e517658cedeab74af891bc49863
kenji-iwatani <kleversonkenji@gmail.com>
2023-05-15 19:07:17 -0300

    chore: adds mui to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { ThemeProvider, Typography } from "@mui/material";
+import defaultTheme from "./data/defaultTheme";
+
 function App() {
-  return <h1>Conteúdo</h1>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Typography>Conteúdo</Typography>
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/data/defaultTheme.ts
@@ -0,0 +1,8 @@
+import { createTheme } from "@mui/material";
+
+export default createTheme({
+  typography: {
+    fontSize: 13,
+    fontFamily: "'Montserrat', sans-serif;",
+  },
+});
