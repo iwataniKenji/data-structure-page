f15cbefebead7a2522b16c68c4335801c637e75a
kenji-iwatani <kleversonkenji@gmail.com>
2023-06-22 17:31:54 -0300

    feat: finishes queue section first part

--- src/pages/QueueSection.tsx
+++ src/pages/QueueSection.tsx
@@ -1,6 +1,100 @@
 import { Box, Container, Typography } from "@mui/material";
+import { ImageWrapper } from "../components/ImageWrapper";
+import { CodeBoard } from "../components/CodeBoard";
+import queue01 from "../assets/queue-01.png";
 
 export function QueueSection() {
+  const queueExample = `  #include <stdio.h>
+  #include <stdlib.h>
+
+  typedef struct Node {
+    int data;
+    struct Node* next;
+  } Node;
+
+  typedef struct Queue {
+    Node* front; // aponta para o primeiro nó da fila
+    Node* rear;  // aponta para o último nó da fila
+  } Queue;
+
+  // cria novo nó
+  Node* createNode(int data) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    
+    newNode->data = data;
+    newNode->next = NULL;
+    
+    return newNode;
+  }
+
+  // cria uma nova fila vazia
+  Queue* createQueue() {
+    Queue* queue = (Queue*)malloc(sizeof(Queue));
+    
+    queue->front = NULL;
+    queue->rear = NULL;
+    
+    return queue;
+  }
+
+  // verifica se a fila está vazia (ou seja, se o front é NULL)
+  int isEmpty(Queue* queue) {
+    return (queue->front == NULL);
+  }
+
+  // adiciona elemento no final
+  void enqueue(Queue* queue, int data) {
+    Node* newNode = createNode(data);
+
+    // se fila estiver vazia, elemento será o primeiro e último
+    if (isEmpty(queue)) {
+      queue->front = newNode;
+      queue->rear = newNode;
+
+      return;
+    }
+
+    // adiciona elemento no final
+    queue->rear->next = newNode;
+    queue->rear = newNode;
+  }
+
+  // remove elemento do início
+  void dequeue(Queue* queue) {
+    if (isEmpty(queue)) {
+      printf("A fila está vazia.");
+      return -1; // retorno que indica erro
+    }
+
+    Node* temp = queue->front; // temporário recebe o primeiro da fila
+    queue->front = queue->front->next;
+
+    // caso o nó removido seja o último nó da fila, atualiza o rear para NULL
+    if (queue->front == NULL) {
+      queue->rear = NULL;
+    }
+
+    // libera memória do nó removido
+    free(temp);
+  }
+
+  int main() {
+    Queue* queue = createQueue();
+
+    enqueue(queue, 10);
+    enqueue(queue, 20);
+    enqueue(queue, 30);
+    
+    // fila atual: 10 (primeiro), 20 (segundo) e 30 (terceiro)
+
+    dequeue(queue); // retira 10
+    dequeue(queue); // retira 20
+    
+    // fila atual: 30 (único elemento)
+
+    return 0;
+  }`;
+
   return (
     <Container maxWidth="lg">
       <Box sx={{ display: "flex", flexDirection: "column", gap: 3 }}>
@@ -14,30 +108,21 @@ export function QueueSection() {
             permitem a inserção de elementos no final da fila e a remoção no
             início da fila, tornando-as adequadas para situações em que a ordem
             de chegada é importante, como o processamento de tarefas em um
-            sistema.
+            sistema. Um exemplo para fácil visualização desse tipo de
+            funcionamento são as filas de bancos, onde os clientes são atendidos
+            de acordo com a ordem de chegada.
           </Typography>
-          <Typography>
-            Uma fila é composta por uma sequência de elementos, onde a inserção
-            ocorre no final da fila e a remoção ocorre no início da fila. Os
-            novos elementos são adicionados ao final da fila, enquanto a remoção
-            ocorre sempre a partir do elemento que está no início. Dessa forma,
-            o primeiro elemento inserido é o primeiro a ser removido, mantendo a
-            ordem FIFO.
-          </Typography>
-
           <Typography>
             A operação de inserção em uma fila é chamada de "enqueue", enquanto
             a operação de remoção é chamada de "dequeue". O "enqueue" adiciona
             um elemento ao final da fila, enquanto o "dequeue" remove o elemento
             do início da fila, expondo o próximo elemento na ordem de chegada.
           </Typography>
-          <Typography>
-            Além disso, as filas podem oferecer a operação "peek", que permite
-            acessar o elemento no início da fila sem removê-lo. Essa operação é
-            útil para verificar o próximo elemento a ser processado sem
-            modificar a estrutura da fila.
-          </Typography>
+          <ImageWrapper imageUrl={queue01} />
 
+          <Typography sx={{ fontSize: 32, fontWeight: 500 }}>
+            Utilização
+          </Typography>
           <Typography>
             As filas são amplamente utilizadas em diversas aplicações, como
             sistemas de gerenciamento de tarefas, algoritmos de busca e
@@ -47,17 +132,15 @@ export function QueueSection() {
             um ponteiro para o próximo elemento.
           </Typography>
           <Typography>
-            Uma variante das filas é a fila de prioridade, na qual cada elemento
-            possui uma prioridade associada. Os elementos são processados de
-            acordo com sua prioridade, permitindo que elementos com prioridade
-            mais alta sejam removidos antes dos de prioridade mais baixa.
+            Segue abaixo um exemplo com listas ligadas do qual o elemento de
+            estrutura Queue (fila) gerencia quem é o último e o primeiro nó,
+            possibilitando o uso dos métodos de enqueue e dequeue para
+            manipulação dos elementos da fila.
           </Typography>
-          <Typography>
-            É importante mencionar que as filas têm uma complexidade de tempo
-            eficiente para as operações básicas "enqueue" e "dequeue", com tempo
-            constante O(1). No entanto, o acesso direto a um elemento em uma
-            posição específica na fila tem complexidade de tempo O(n), onde n é
-            o número de elementos na fila.
+
+          <CodeBoard codeSnippet={queueExample} />
+          <Typography sx={{ fontSize: 32, fontWeight: 500 }}>
+            Conclusão
           </Typography>
           <Typography>
             Em resumo, as filas são estruturas de dados que seguem o princípio
